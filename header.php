f37ec0e2f2c5ad21b971cce0e88d46e7dc81ed7c
Valentine APPY <vale.appy@gmail.com>
2019-12-01 14:20:40 +0100

    [V] - 01/12/2019 - Ajout d'extensions

---
+++ header.php
@@ -0,0 +1,26 @@
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+  <meta name="description" content="Capsee"/>
+
+  <title>Capsee</title>
+
+<!--Logos -->
+  <link rel="shortcut icon" type="image/x-icon" type="image/ico" href="#" />
+  <link rel="apple-touch-icon" sizes="60x60 76x76 120x120 152x152" type="image/png" href="" />
+
+<!-- Ajout d'extensions -->
+  <script type="text/javascript" src="../ext/jquery-3.3.1.min.js"></script>
+
+  <script type="text/javascript" src="../ext/jquery.min.js"></script>
+  <script type="text/javascript" src="../ext/moment.min.js"></script>
+  <script type="text/javascript" src="../ext/bootstrap/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="../ext/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+  <script type="text/javascript" src="../ext/bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js"></script>
+  <link rel="stylesheet" href="../ext/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../ext/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css"  />
+  <link rel="stylesheet" href="../ext/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css"  />
+  <!--<link rel="stylesheet" href="../css/style.css">-->
+
+</head>
