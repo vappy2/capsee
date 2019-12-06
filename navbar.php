a63b1c817bdfb0603df2a6f638329764bc8d3def
Valentine APPY <vale.appy@gmail.com>
2019-12-06 23:57:22 +0100

    [V] - 06/12 - Interface de création de capsule V1

---
+++ navbar.php
@@ -0,0 +1,7 @@
+
+<nav class="navbar">
+    <a class="navbar-brand" href="#"><img src="img/capsee_b_header.png"></a>
+    <ul class="navbar-nav">
+        <li><a href="#"><img src="img/pictos/user_b_02@2x.png"></a></li>
+    </ul>            
+</nav>
