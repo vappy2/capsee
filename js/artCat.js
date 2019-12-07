261dae0a5979bcefcf1338c8b08e32a374cde155
Valentine APPY <vale.appy@gmail.com>
2019-12-07 16:41:46 +0100

    [V] - 06/12 - Catégories d'Art & Form Films v1

---
+++ js/artCat.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("artAjoute li").click(function(){
+        $("artAjoute li").css('border','2px solid #28E0AE');
+        $("#artValider").css('background-color','#28E0AE');
+    });
+});
